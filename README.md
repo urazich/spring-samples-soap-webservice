94931ef3b6c1f18c592fe50937e1567ca868bb21
Damir Urazov <urazovdamir@gmail.com>
2019-08-30 23:43:58 +0600

    added spring framework with dependencies for wsdl service added plugin jaxb2-maven-plugin for generation java objects from XSD

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring-samples-soap-webservice
 Test app with a SOAP service
+
+App was created by a guide: https://spring.io/guides/gs/producing-web-service/
---
+++ src/main/java/kz/urazov/spring/sample/soapwebservice/SoapWebserviceApplication.java
@@ -0,0 +1,13 @@
+package kz.urazov.spring.sample.soapwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapWebserviceApplication.class, args);
+    }
+
+}
